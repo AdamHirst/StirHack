ef28e04c7c05910fa3b4a980f00f8fde0097951b
samwalls <sam@s-w.io>
2017-02-12 04:29:01 +0000

    add timestamp to face detector

--- preproc/tst.py
+++ preproc/tst.py
@@ -3,10 +3,11 @@ import cv2
 
 fd = video.FaceDetector()
 fd.openCapture()
-ok, frame, faces = fd.hasFace()
+ok, frame, faces, time = fd.hasFace()
 print ok
 print frame
 print faces
+print time
 if ok:
 	print frame
 	cv2.imshow('img', frame)
--- preproc/video.py
+++ preproc/video.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 
 class FaceDetector:
 
@@ -30,17 +31,18 @@ class FaceDetector:
 		self.closeCapture()
 		self.openCapture()
 
-	# returns true, along with the image in question, and a list of x,y,w,h
-	# tuples, if the main camera detects a face - if the main camera fails to
-	# grab a frame or does not detect a face then false is returned
+	# returns true, along with the image in question, a list of x,y,w,h
+	# tuples, and the timestamp that the image was taken on if the main camera
+	# detects a face - if the main camera fails to grab a frame or does not
+	# detect a face then false is returned
 	def hasFace(self):
 		read, frame = self.mainCapture.read()
 		if not read:
-			return False, [[[]]], ()
+			return False, [[[]]], (), int(time.time())
 		# find out if the frame contains any faces
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		faces = self.__face_cascade.detectMultiScale(gray, 1.1, 2)
 		if len(faces) > 0:
-			return True, frame, faces
+			return True, frame, faces, int(time.time())
 		else:
-			return False, [[[]]], ()
+			return False, [[[]]], (), int(time.time())
